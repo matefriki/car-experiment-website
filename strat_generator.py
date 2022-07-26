6ee51227ea1e1144aa292635f90519b77018ce09
kbjorner <89598922+kbjorner@users.noreply.github.com>
2022-07-26 10:27:15 +0200

    comments, cleaned up file

--- strat_generator.py
+++ strat_generator.py
@@ -1,75 +1,78 @@
 import re
 import json
 
+# regex used to identify elements to change in original PRISM file
 label_reg = re.compile("\/\/\s*{([a-zA-Z_]+)}")
-file_label = re.compile("/(mdp)/")
 strat_regex = "\[[\w-]+\]"
 strategies_label = re.compile(strat_regex)
-strat_name_label = re.compile("[\w-]+")
 
-
-def make_mdp():
-    file = open(input("Model file to convert to template: "), "r")
+# generates runnable mdp file
+def make_mdp(temp):
+    file = open(temp, "r")
     lines = [line.strip() for line in file.readlines()]
     file.close()
     
     for i, line in enumerate(lines):
         label_match = label_reg.search(line)
+        # compatible with "".format for changing variables from user input
         if label_match:
-            # print(f"Match: {line}")
             start_ind = line.rfind("init") + 4 if "init" in line else line.rfind("=") + 1
             label = label_match.group(1)
             lines[i] = line[:start_ind] + " {{{}}};".format(label)
         strat_match = strategies_label.search(line)
+        # resets car action labels to [], originally set to label for importing strategies
         if strat_match:
-            # print(f"stratmatch: {line}")
             lines[i] = re.sub(strat_regex, "[]", lines[i])
 
+    # rewrites lines into new file
     replaced = '\n'.join(lines)
     with open("mdpgenerated.pm", "w") as f:
         f.write(replaced)
 
-
+# generates DTMC files for a dictionary and file input
 def make_dtmc(dict, temp):
     file = open(temp, "r")    
     lines = [line.strip() for line in file.readlines()]
     file.close()
 
-    print(dict)
-    # print(inner)
-    # for key in dict:
-    #     print(f"key: {key}") # cautious, risky
-    #     print(f"dict[key]: {dict[key]}") # the list of acc, br, nop in each "key" = strategy type
-    # key = [key for key in dict]
-    # change file type from mdp to dtmc
+  
     for i, line in enumerate(lines):
+        # change file type from mdp to dtmc
         lines[i] = re.sub("mdp", "dtmc", lines[i])
         label_match = label_reg.search(line)
+        # compatible with "".format for changing variables from user input
         if label_match:
-            # print(f"Match: {line}")
             start_ind = line.rfind("init") + 4 if "init" in line else line.rfind("=") + 1
             label = label_match.group(1)
             lines[i] = line[:start_ind] + " {{{}}};".format(label)
+        # finds strategy labels in PRISM file "[accelerate]..." and adds in corresponding guard
         strat_match = strategies_label.search(line)
         if strat_match:
             for key in dict:
-               
-                # print(f"i: {key}")
-                # print(f"dict: {dict}")
-                # if strat_match.group(0) == key:
                 if strat_match.group(0) == key: 
                     print(f"dict[key]: {dict[key]}")
                     lines[i] = re.sub(strat_regex, f"[] {dict[key]}  & ", lines[i]) 
-            
+    # rewrites lines into new files
     replaced = '\n'.join(lines)
     with open(f"dtmc{strat}.pm", "w") as f:
         f.write(replaced)
 
 
+def main():
+    strats = open('strategy.json')
+    strategy = json.load(strats)
 
-# # make_mdp()
-strats = open('strategy.json')
-strategy = json.load(strats)
-for strat in strategy:
-# for i in range(1):
-    make_dtmc(strategy[strat], "test.pm")
+    # change this to final PRISM file
+    prism_file = "prism_files/test.pm"
+
+    # makes ones mdp file
+    make_mdp(prism_file)
+
+    # makes one dtmc file per strategy listed in the json file
+    for strat in strategy:
+        make_dtmc(strategy[strat], prism_file)
+
+if __name__ == '__main__':
+    main()
+else:
+    print("No PRISM files generated")
