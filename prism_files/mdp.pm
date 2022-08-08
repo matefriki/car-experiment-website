a433673446ec38b373c3514850fa882d0cb743f5
Filip Cano <filip.cano@iaik.tugraz.at>
2022-08-08 14:23:11 +0200

    clean up mdp file

--- prism_files/mdp.pm
+++ prism_files/mdp.pm
@@ -4,19 +4,13 @@ const int street_length = 100;
 const int min_street_length = 50;
 const int sidewalk_height = 2;
 
-const int crosswalk_pos = 80;
+const int crosswalk_pos = 70;
 const int crosswalk_width = 10;
 const int crosswalk_height = 11;
 
 const int world_height = (sidewalk_height * 2) + crosswalk_height;
 
-const int max_dist = (street_length * street_length) + (world_height * world_height) + 100;
-
 const int max_speed = 5;
-
-const double neutral = 0.5;
-const double change_prob1 = 0.7;
-const double change_prob2 = 1 - change_prob1;
 
 // block properties
 const int block_height = 2;
@@ -42,8 +36,8 @@ formula crash = ((ped_x >= car_x) & (ped_x <= car_x + car_width)) & ((ped_y >= c
 label "crash" = crash;
 label "givereward" = ((finished=0) & (car_x = street_length));
 
-// checks distances of pedestrian to car and vis line intersections from the car to the block
-label "blocked_vis" = (dist_ped >= min(dist_s1, dist_s2, dist_s3, dist_s4));
+formula dist_ped = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1));
+formula car_fast = (dist_ped <= ((car_v*car_v) + car_v)/2);
 
 formula dist = max(ped_x-car_x, car_x - ped_x) + max(ped_y - car_y, car_y - ped_y);	
 formula safe_dist = dist > 15;
@@ -56,43 +50,6 @@ formula y1 = car_y;
 formula x2 = ped_x;
 formula y2 = ped_y;
 
-formula dist_ped = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1));
-
-// block dist to ped
-formula b1 = block_x2 - block_x1;
-formula b2 = block_y2 - block_y1;
-
-formula ped_to_block = ((x2 - b1)*(x2 - b1)) + ((y2 - b2)*(y2 - b2));
-formula car_to_block = ((x1 - b1)*(x1 - b1)) + ((y1 - b2)*(y1 - b2));
-
-formula vis_blocked = car_to_block > ped_to_block;
-
-formula int_s1 = (((y2 -  y1)/(x2 - x1))*(block_x1 - x1) + y1); // x = block_x1
-formula int_s2 = (((x2 - x1)/(y2 - y1))*(block_y2 - y1) + x1); // y = block_y2
-formula int_s3 = (((y2 -  y1)/(x2 - x1))*(block_x2 - x1) + y1); // x = block_x2
-formula int_s4 = (((x2 - x1)/(y2 - y1))*(block_y1 - y1) + x1); // y = block_y1
-
-// checks if there are intersections of the visibility line from car to pedestrian at each side of the block
-formula s1 = (int_s1 > block_y1) & (int_s1 < block_y2); // (block_x1, int_s1)
-formula s2 = (int_s2 > block_x1) & (int_s2 < block_x2); // (int_s2, block_y2)
-formula s3 = (int_s3 > block_y1) & (int_s3 < block_y2); // (block_x2, int_s3)
-formula s4 = (int_s4 > block_x1) & (int_s4 < block_x2); // (int_s4, block_y1)
-
-// distance formulas for car to pedestrian and each intersection (above)
-formula dist_s1 =  s1 ? ((block_x1 - x1)*(block_x1 - x1)) + ((int_s1 - y1)*(int_s1 - y1)) : max_dist;
-formula dist_s2 = s2 ? ((int_s2 - x1)*(int_s2 - x1)) + ((block_y2 - y1)*(block_y2 - y2)) : max_dist;
-formula dist_s3 = s3 ? ((block_x2 - x1)*(block_x2 - x1)) + ((int_s3 - y1)*(int_s3 - y1)) : max_dist;
-formula dist_s4 = s4 ? ((int_s4 - x1)*(int_s4 - x1)) + ((block_y1 - y1)*(block_y1 - y1)) : max_dist;
-
-formula car_fast = (dist_ped <= ((car_v*car_v) + car_v)/2);
-formula ped_vis = (dist_ped < min(dist_s1, dist_s2, dist_s3, dist_s4));
-
-formula intersection = (s1 | s2 | s3 | s4);
-
-formula betweencb = (ped_to_block < car_to_block) & (ped_vis);
-
-formula vis_is_blocked = (dist_ped > ped_to_block)&(dist_ped > car_to_block);
-formula vis_not_blocked = (dist_ped < ped_to_block)&(car_to_block < ped_to_block);
 
 // new visibility criteria
 formula car_left = (x1 < block_x1);
@@ -113,7 +70,7 @@ formula top_blocked = car_top & ped_down & !same_side2;
 formula no_vis = left_blocked | top_blocked;
 
 module Car
-car_x : [min_street_length..street_length] init 5; //{car_x};
+car_x : [min_street_length..street_length] init 55; //{car_x};
 car_v : [0..max_speed] init 0;
 visibility : [0..1] init 1;
 finished : [0..1] init 0;
@@ -174,7 +131,7 @@ module Pedestrian
 // conditions for ped to start crossing the street
 	// 2.a 40% probability of crossing street when at the crosswalk
 	// 30% chance of walking left or right is it doesn't cross the street
-	[] (turn = 2)&(!ped_vis)&(ped_x >= crosswalk_pos)&(ped_x <= (crosswalk_pos + crosswalk_width))&(is_on_sidewalk) -> // !car_fast)&
+	[] (turn = 2)&(visibility=0)&(ped_x >= crosswalk_pos)&(ped_x <= (crosswalk_pos + crosswalk_width))&(is_on_sidewalk) -> // !car_fast)&
 		0.4: (ped_y' = min(ped_y + 1, world_height))&(turn' = 0) + // Up
 		0.3: (ped_x' = max(ped_x - 1, min_street_length))&(turn' = 0) + // Left
 		0.3: (ped_x' = min(ped_x + 1, street_length))&(turn' = 0); // Right
@@ -182,7 +139,7 @@ module Pedestrian
 	// 2.b 10% chance of crossing the street given the ped can see the car and is a certain distance away from the car
 	// and is at the crosswalk
 	// 90% chance of doing other things
-	[] (turn = 2)&(ped_vis)&(ped_x >= crosswalk_pos)&(ped_x <= (crosswalk_pos + crosswalk_width))&(is_on_sidewalk) -> //(car_fast)
+	[] (turn = 2)&(visibility=1)&(ped_x >= crosswalk_pos)&(ped_x <= (crosswalk_pos + crosswalk_width))&(is_on_sidewalk) -> //(car_fast)
 		0.1: (ped_y' = min(ped_y + 1, world_height))&(turn' = 0) + // Up
 		0.45: (ped_x' = max(ped_x - 1, min_street_length))&(turn' = 0) + // Left
 		0.45: (ped_x' = min(ped_x + 1, street_length))&(turn' = 0); // Right
@@ -206,7 +163,3 @@ module Pedestrian
 		0.4*0.01: (ped_x' = min(ped_x + 1, street_length))&(turn' = 0); // Right
 
 endmodule
-
-rewards
-	[] (finished=0) & (car_x = street_length) : 10;
-endrewards
