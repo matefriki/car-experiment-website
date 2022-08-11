c31e8f549b6a2ad8807423595c9b5a9e969dd976
Lukas Posch <lukas.posch@student.tugraz.at>
2022-08-11 09:59:37 +0200

    set work dir, add checks for file i/o

--- prism_runner.py
+++ prism_runner.py
@@ -1,8 +1,10 @@
 from asyncio import base_tasks
-from os import system
 from time import sleep
 import json
 import sys
+import os
+
+# set working directory - this is necessary on the server
 
 # Ranges for all state variables (inclusive) in order of input
 ranges = [
@@ -18,8 +20,11 @@ ranges = [
 ]
 
 # Get path to prism directory from config.txt
-with open("config.txt", "r") as file:
-    prism_path = file.read().strip()
+try:
+	with open("config.txt", "r") as file:
+	    prism_path = file.read().strip()
+except Exception as e:
+	sys.exit("Cannot open file config.txt, error: {e}, working dir = " + directory)
 
 # Split arguments into array of strings
 starting_state = [arg for arg in input().split(" ") if arg]
@@ -41,7 +46,7 @@ try:
     starting_state = [int(n) for n in starting_state]
 # Ensure all arguments parse correctly
 except Exception as e:
-    sys.exit(f"Parse error: {e}")
+    sys.exit("Parse error: {e}")
 
 # Ensure arguments are within desired ranges
 def within_range(num, range):
@@ -51,31 +56,39 @@ if not all([within_range(arg, ranges[i]) for i, arg in enumerate(starting_state)
 
 path_length, person_x, person_y, car_x, car_y, top_corner_x, top_corner_y, bottom_corner_x, bottom_corner_y = starting_state
 
-with open("template.pm", "r") as file:
-    template = file.read()
+try:
+	with open("template.pm", "r") as file:
+		template = file.read()
+except Exception as e:
+	sys.exit("Cannot open file template.pm, error: {e}")
 
 program = template.format(person_x = person_x, person_y = person_y, car_x = car_x, car_y = car_y, top_corner_x = top_corner_x, top_corner_y = top_corner_y,  bottom_corner_x = bottom_corner_x, bottom_corner_y = bottom_corner_y)
-with open("program.pm", "w") as file:
-    file.write(program)
+try:
+	with open("program.pm", "w") as file:
+		file.write(program)
+except Exception as e:
+	sys.exit("Cannot open file program.pm, error: {e}")
 sleep(.1)
 
-system("{} program.pm -simpath {} path.txt >/dev/null 2>&1".format(prism_path, path_length))
+os.system("{} program.pm -simpath {} path.txt >/dev/null 2>&1".format(prism_path, path_length))
 
 def load_path(file_name):
-    file = open(file_name, "r")
-    path_lines = [x.strip() for x in file.readlines()]
-    file.close()
+	try:
+		with open(file_name, "r") as file:
+			path_lines = [x.strip() for x in file.readlines()]
+	except Exception as e:
+		sys.exit("Cannot open file {file_name}, error: {e}")
 
     # make each array item into sep array
-    path_lines = [item.split(" ") for item in path_lines]
-    labels = path_lines[0]
-    path_lines = path_lines[1:]
+	path_lines = [item.split(" ") for item in path_lines]
+	labels = path_lines[0]
+	path_lines = path_lines[1:]
 
-    path = {}
-    for i, label in enumerate(labels):
-        path[label] = [row[i] for row in path_lines]
+	path = {}
+	for i, label in enumerate(labels):
+		path[label] = [row[i] for row in path_lines]
 
-    return path
+	return path
 
 sleep(.1)
 path = load_path("path.txt")
