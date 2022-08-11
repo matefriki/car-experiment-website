433e77a2c5f3f477a82056c175a038cd19af1f33
Lukas Posch <lukas.posch@student.tugraz.at>
2022-08-11 09:59:45 +0200

    add som logging to main.js

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ function getPrismPath() {
   return buffer.toString()
 }
 
+app.get('/info', (req, res) => {
+  res.send('Crosswalk');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -35,22 +39,49 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('generate', (path_length, person_x, person_y, car_x, car_y, top_corner_x, top_corner_y, bottom_corner_x, bottom_corner_y) => {
     queue.push((closed) => {
-      const runner = spawn('python3', ['prism_runner.py'], { timeout: 5000 });
+    
+    	console.log('get input parameters: ', path_length, person_x, person_y, car_x, car_y, top_corner_x, top_corner_y, bottom_corner_x, bottom_corner_y)
+    	
+      const runner = spawn('python3', [`${__dirname}/prism_runner.py`], { 
+        timeout: 5000 
+      });
+
+      
+      runner.on('error', (err) => {
+				console.error(`prism runner: failed to start with error code ${err}`);
+			});
+			
+			console.log('prism runner: started');
+      console.log(`arguments for runner stdin are: ${path_length} ${person_x} ${person_y} ${car_x} ${car_y} ${top_corner_x} ${top_corner_y} ${bottom_corner_x} ${bottom_corner_y}`);
+      
       runner.stdin.write(`${path_length} ${person_x} ${person_y} ${car_x} ${car_y} ${top_corner_x} ${top_corner_y} ${bottom_corner_x} ${bottom_corner_y}`);
       runner.stdin.end();
 
       // Must have buffer because chunk size from python is smaller than full path (over path_length of 100)
-      let buffer = "";
+      let buffer = "";    
+       
+      runner.stdout.setEncoding('utf8');
       runner.stdout.on('data', (data) => {
+        console.log(`prism runner: write data from stdout to buffer`);
         buffer += data.toString();
-        console.log("data");
       });
+      
+			runner.stderr.setEncoding('utf8');
+			runner.stderr.on('data', (data) => {
+					console.log(`stderr: ${data}`);
+					buffer += data.toString();
+			});
 
-      runner.on('close', () => {
+      runner.on('close', (code) => {
         socket.emit("path", buffer);
-        console.log('close');
+        console.log(`prism runner: closed with code ${code}`);
         closed();
       });
+      
+      runner.on('exit', (code) => {
+				console.log(`prism runner: exited with code ${code}`);
+			});
+      
     });
 
     handleQueue();
